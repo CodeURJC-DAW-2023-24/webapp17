bfad61395ec18e2099c54f533f7c6a0b18392556
ggronda <jesusggironda@gmail.com>
2024-05-01 14:17:18 +0200

    Maven project created

---
+++ demo/src/test/java/www/webapp17/urjc/AppTest.java
@@ -0,0 +1,20 @@
+package www.webapp17.urjc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
