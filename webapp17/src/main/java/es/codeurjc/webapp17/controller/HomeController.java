4970eba78cadfb547cf3bac7d36c5b624ade24bd
ggronda <jesusggironda@gmail.com>
2024-09-11 18:58:41 +0200

    Ocultación y permisos de borrado de post/comentarios

--- webapp17/src/main/java/es/codeurjc/webapp17/controller/HomeController.java
+++ webapp17/src/main/java/es/codeurjc/webapp17/controller/HomeController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 import es.codeurjc.webapp17.entity.Post;
-
+import es.codeurjc.webapp17.entity.Usr;
 import es.codeurjc.webapp17.service.PostService;
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
@@ -22,9 +23,19 @@ public class HomeController {
 
     
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model,HttpSession session) {
         ArrayList<Post> posts = (ArrayList<Post>) postService.getAllPost();
         model.addAttribute("posts", posts);
+        Usr user = (Usr) session.getAttribute("user");
+        if (user != null) {
+            if (user.getRole() == Usr.Role.ADMIN) {
+                model.addAttribute("ADMIN", true);
+                return "index";
+            }
+            }else {
+                model.addAttribute("ADMIN", false);
+            
+        }
         return "index"; // Devuelve la plantilla index.html
     }
 
